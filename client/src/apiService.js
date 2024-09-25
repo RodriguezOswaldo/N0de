a436e4ca109efcb49638ba64a9cd35a290161b1f
Oswaldo Rodriguez <Owner2303@gmail.com>
2024-09-25 06:25:55 -0400

    this

--- client/src/apiService.js
+++ client/src/apiService.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 const API_URL = 'https://localhost:4000/api'
-
 export const getAttendance =()=> axios.get(`${API_URL}/attendance`);
 export const getProjects =()=> axios.get(`${API_URL}/projects`);
 export const getCurriculums =()=> axios.get(`${API_URL}/curriculums`);
